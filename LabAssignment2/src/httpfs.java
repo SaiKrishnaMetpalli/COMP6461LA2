f9a517e52c15821d9e2d2040d907585407040ee8
Sai Krishna Metpalli <saikrishnametpalli@gmail.com>
2020-11-03 11:53:54 -0500

    changed the args length

--- LabAssignment2/src/httpfs.java
+++ LabAssignment2/src/httpfs.java
@@ -1,14 +1,9 @@
 
 public class httpfs {
 
-	public static void main(String[] args) {
-		if (args.length != 0) {
-			HttpServerLibrary http_Server = new HttpServerLibrary(args);
-			http_Server.handleCommand();
-		} else {
-			System.out.println("\n==========Invalid Command");
-		}
-
+	public static void main(String[] args) {		
+		HttpServerLibrary http_Server = new HttpServerLibrary(args);
+		http_Server.handleCommand();		
 	}
 
 }
