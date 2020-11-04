74e428f9bb52bf94c63884167c1eac3bf26f444b
rohanvachhani <rohan.vachhani5@gmail.com>
2020-11-03 19:52:42 -0500

    code cleaned

--- LabAssignment2/src/ClientRequestHandler.java
+++ LabAssignment2/src/ClientRequestHandler.java
@@ -1,32 +1,21 @@
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.HashMap;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStreamWriter;
 
 public class ClientRequestHandler implements Runnable {
 
 	private Socket client_socket;
-	private boolean print_Debug_Message;
-	private String directory_Path;
-	// BufferedWriter b_writer = null;
+	private boolean is_debug_on;
+	private String dir_path;
 	private BufferedReader b_reader;
 	private BufferedWriter b_writer;
 
 	private String header = "";
 	private String file_path = null;
-	private String file_name = null;
 
 	private String contentType = "";
 	private String contentDisposition = "";
@@ -34,12 +23,9 @@ public class ClientRequestHandler implements Runnable {
 	// deal with request
 	private boolean listAllFiles = false;
 
-
-	// generate respond
-	// private String contentType = "plain/text";
 	private int statusCode = 200;
 	private StringBuilder respond = new StringBuilder();
-	private StringBuilder respondBody = new StringBuilder();
+	private StringBuilder main_response_data = new StringBuilder();
 
 	private int contentLength = 0;
 
@@ -47,8 +33,8 @@ public class ClientRequestHandler implements Runnable {
 			BufferedWriter bw) {
 
 		this.client_socket = client;
-		this.print_Debug_Message = print_Debug_Message;
-		this.directory_Path = directory_Path;
+		this.is_debug_on = print_Debug_Message;
+		this.dir_path = directory_Path;
 		this.b_reader = br;
 		this.b_writer = bw;
 
@@ -60,30 +46,30 @@ public class ClientRequestHandler implements Runnable {
 
 			// read request of client
 			read_request();
-			// Dealing with request
-			if (print_Debug_Message == true) {
-				System.out.println("\n-------------------- Dealing with request... -----------------\n");
-				System.out.println("Method:              " + header);
+			// Processing the request
+			if (is_debug_on == true) {
+				System.out.println("\n---------- Processing the request...\n");
+				System.out.println("Type of the method : " + header);
 			}
 
-			dealWithRequest();
+			process_request();
 
-			// Generate responds
-			if (print_Debug_Message)
-				System.out.println("\n-------------------- Generate respond... ---------------------\n");
-			generateRespond();
+			// making response for request
+			if (is_debug_on)
+				System.out.println("\n---------- Making response...\n");
+			make_response();
 
-			// send responds to client
-			if (print_Debug_Message)
-				System.out.println("\n-------------------- Send respond to client... ---------------\n");
+			// sending response to the client
+			if (is_debug_on)
+				System.out.println("\n---------- Sending response to the client...\n");
 
 			b_writer.write(respond.toString() + "\n");
 
 			b_writer.flush();
 
-			if (print_Debug_Message)
-				System.out.println("\n-------------------- Finish... -------------------------------\n");
-			// Thread.sleep(1000);
+			if (is_debug_on)
+				System.out.println("\n----------response sent successfully.\n");
+
 			b_writer.close();
 			client_socket.close();
 
@@ -99,11 +85,11 @@ public class ClientRequestHandler implements Runnable {
 
 		while ((line = b_reader.readLine()) != null) {
 
-			if (print_Debug_Message == true) {
+			if (is_debug_on == true) {
 				System.out.println("Current Line from response: " + line);
 			}
-			
-			if(line.equals("")) {
+
+			if (line.equals("")) {
 				break;
 			}
 			// deal with header
@@ -117,8 +103,8 @@ public class ClientRequestHandler implements Runnable {
 				file_path = line.substring(path_start_index, index_of_space);
 
 				if ("/".equals(file_path.trim())) {
-					file_path = directory_Path;
-					//System.out.println("\nListing all Files in directory...");
+					file_path = dir_path;
+					// System.out.println("\nListing all Files in directory...");
 					listAllFiles = true;
 					return;
 				}
@@ -135,103 +121,97 @@ public class ClientRequestHandler implements Runnable {
 //				}
 //
 //			}
-			//System.out.println("\nEnd itr of while loop\n");
-			
+			// System.out.println("\nEnd itr of while loop\n");
+
 		}
-		// Thread.sleep(1000);
 
 	}
 
-	private void dealWithRequest() throws IOException, InterruptedException {
-		// TODO Auto-generated method stub
-		if (200 != statusCode) return;
-        if (file_path.length() > 3 && (file_path.substring(0, 4).equals("/../") || (file_path.substring(0, 4).equals("./..") ))) {
-            statusCode = 403;
-            respondBody.append("You tried to leave the working directory, which is not allowed for security reason.\r\n");
-            return;
-        }
-        
-		if (listAllFiles == true) {
-			// print all files in the current directory
+	private void process_request() throws IOException, InterruptedException {
 
-			
-			print_all_files(file_path, respondBody);
-			
+		if (200 != statusCode)
+			return;
+		if (file_path.length() > 3
+				&& ((file_path.substring(0, 4).equals("./..") || file_path.substring(0, 4).equals("/../")))) {
+			statusCode = 403;
+			main_response_data.append(
+					"Due to security reasons, It is not allowed to access other directories on this server!\r\n");
+			return;
+		}
+
+		if (listAllFiles == true) {
+
+			list_of_all_files(file_path, main_response_data);
 
 		} else {
-			// TODO: when pass is ./ i.e. localhost/././file_1
-			File getFile = new File(directory_Path + file_path.trim());
 
-			String fileType = getFile.toURL().openConnection().getContentType();
-			contentType = fileType;
+			File requested_file = new File(dir_path + file_path.trim());
+
+			contentType = requested_file.toURL().openConnection().getContentType();
+
 			if (contentType.equals("text/plain")) {
 				contentDisposition = "inline";
 			} else {
-				contentDisposition = "attachment; filename=" + directory_Path + file_path + ";";
+				contentDisposition = "attachment; filename=" + dir_path + file_path + ";";
 			}
-			if (print_Debug_Message) {
-				System.out.println("Content Type:        " + fileType);
+			if (is_debug_on) {
 				System.out.println("Content Disposition: " + contentDisposition);
+				System.out.println("Content Type:        " + contentType);
+
 			}
 			if (!contentType.equals("text/plain")) {
-				respondBody.append("File type is ").append(contentType).append(", cannot read byte file.");
-			} else if (getFile.exists() && getFile.isFile()) {
+				main_response_data.append("Cannot read").append(contentType).append(", type of file!");
+			} else if (requested_file.exists() && requested_file.isFile()) {
 
-				BufferedReader getFileContents = new BufferedReader(new FileReader(getFile));
+				BufferedReader data_of_file = new BufferedReader(new FileReader(requested_file));
 				String getLine;
-				while (null != (getLine = getFileContents.readLine())) {
-					respondBody.append(getLine).append("\r\n");
+				while (null != (getLine = data_of_file.readLine())) {
+					main_response_data.append(getLine).append("\r\n");
 				}
 			} else {
-				System.out.println("\nFile exist status: " + getFile.exists());
+				System.out.println("\nFile exist status: " + requested_file.exists());
 				statusCode = 404;
 			}
 		}
-		// Thread.sleep(1000);
 
 	}
 
-	private void print_all_files(String file_path, StringBuilder respondBody) {
-		File curDir = new File(file_path);
-		File[] filesList = curDir.listFiles();
-		
-
+	private void list_of_all_files(String file_path, StringBuilder respondBody) {
+		File current_directory = new File(file_path);
+		File[] filesList = current_directory.listFiles();
 		if (null != filesList) {
 			for (File file : filesList) {
-				//System.out.println("\nFile: "+ file.getName());
 				if (file.isFile()) {
-
 					respondBody.append(file.getName()).append("\r\n");
-
 				}
 			}
 		}
 	}
 
-	private void generateRespond() throws Exception {
-		if (404 == statusCode) {
+	private void make_response() throws Exception {
+		if (statusCode == 404) {
 			respond.append("HTTP/1.1 404 NOT FOUND\r\n");
-			respondBody.append("The requested File was not found on the server.\r\n");
+			main_response_data.append("The requested File was not found on the server.\r\n");
 			// respondBody.append("If you entered the URL manually, please check you
 			// spelling and try again.\r\n");
-		} else if (403 == statusCode) {
+		} else if (statusCode == 403) {
 			respond.append("HTTP/1.1 403 Forbidden\r\n");
-		} else if (400 == statusCode) {
+		} else if (statusCode == 400) {
 			respond.append("HTTP/1.1 40 Bad Request\r\n");
 		} else {
 			// Thread.sleep(1000);
 			respond.append("HTTP/1.1 200 OK\r\n");
 			// TODO: show create/overwrite info
 			if (header.equals("POST"))
-				respondBody.append("Post file successfully.");
+				main_response_data.append("Post file successfully.");
 		}
 		respond.append("Connection: close\r\n");
 		respond.append("Server: httpfs\n");
 		respond.append("Date: ").append(Calendar.getInstance().getTime().toString()).append("\r\n");
 		respond.append("Content-Type: ").append(contentType).append("\r\n");
-		respond.append("Content-Length: ").append(respondBody.length()).append("\r\n");
+		respond.append("Content-Length: ").append(main_response_data.length()).append("\r\n");
 
-		respond.append(respondBody.toString());
+		respond.append(main_response_data.toString());
 
 	}
 
