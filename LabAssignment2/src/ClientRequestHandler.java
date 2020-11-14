291066221c80cd0688c2f8383568acf81acdfdf7
rohanvachhani <rohan.vachhani5@gmail.com>
2020-11-13 21:11:28 -0500

    json handled

--- LabAssignment2/src/ClientRequestHandler.java
+++ LabAssignment2/src/ClientRequestHandler.java
@@ -20,12 +20,12 @@ public class ClientRequestHandler implements Runnable {
 	private String content_Type = "";
 	private String content_Disposition = "";
 	private boolean list_All_Files = false;
-	private boolean is_Overwrite=false;
+	private boolean is_Overwrite = false;
 	private int status_Code = 200;
 	private StringBuilder respond = new StringBuilder();
 	private StringBuilder main_Response_Data = new StringBuilder();
-	String data="";
-	
+	String data = "";
+
 	public ClientRequestHandler(Socket client, boolean print_Debug_Message, String directory_Path, BufferedReader br,
 			BufferedWriter bw) {
 
@@ -88,49 +88,48 @@ public class ClientRequestHandler implements Runnable {
 			if (line.equals("")) {
 				break;
 			}
-			
+
 			// deal with method
 			if (line.toLowerCase().contains("get")) {
 				method = "get";
 				int path_start_index = line.indexOf("/"); // start searching "/" after "http://" in "GET /files.txt
-								// HTTP/1.0" (our received line) should return 4
+				// HTTP/1.0" (our received line) should return 4
 				int index_of_space = line.indexOf(" ", path_start_index);
 				file_Path = line.substring(path_start_index, index_of_space);
-			}			
-			else if (line.toLowerCase().contains("post")) {
+			} else if (line.toLowerCase().contains("post")) {
 				method = "post";
 				int path_start_index = line.indexOf("/"); // start searching "/" after "http://" in "GET /files.txt
-								// HTTP/1.0" (our received line) should return 4
+				// HTTP/1.0" (our received line) should return 4
 				int index_of_space = line.indexOf(" ", path_start_index);
 				file_Path = line.substring(path_start_index, index_of_space);
-			}			
-			
+			}
+
 			if ("/".equals(file_Path.trim())) {
 				file_Path = dir_Path;
 				list_All_Files = true;
 				return;
-			}			
-			
-			if((method.equals("post")) && (line.contains("Content-Length"))) {				
-				line=b_Reader.readLine();
-				if(line.contains("Overwrite")) {					
-					is_Overwrite=true;
-					line=b_Reader.readLine();
-				}
-				data=line;
-				if(is_Debug_On) {
-					System.out.println("Storing the data from headers");
-				}				
 			}
-			
+
+			if ((method.equals("post")) && (line.contains("Content-Length"))) {
+				line = b_Reader.readLine();
+				if (line.contains("Overwrite")) {
+					is_Overwrite = true;
+					line = b_Reader.readLine();
+				}
+				data = line;
+				if (is_Debug_On) {
+					System.out.println("Storing the data from headers");
+				}
+			}
+
 		}
 	}
 
 	private void processRequest() throws IOException, InterruptedException {
-		
+
 		if (200 != status_Code)
 			return;
-		
+
 		if (file_Path.length() > 3
 				&& ((file_Path.substring(0, 4).equals("./..") || file_Path.substring(0, 4).equals("/../")))) {
 			status_Code = 403;
@@ -138,29 +137,34 @@ public class ClientRequestHandler implements Runnable {
 					"Due to security reasons, It is not allowed to access other directories on this server!\r\n");
 			return;
 		}
-		
+
 		if (list_All_Files == true) {
 			listOfAllFiles(file_Path, main_Response_Data);
 		} else {
-			if(method.equals("get")) {
+			if (method.equals("get")) {
 				File requested_file = new File(dir_Path + file_Path.trim());
 
 				content_Type = requested_file.toURI().toURL().openConnection().getContentType();
+				
+				if (requested_file.getName().endsWith(".json")) {
+					content_Type = "application/json";
+				}
 
 				if (content_Type.equals("text/plain")) {
 					content_Disposition = "inline";
-					main_Response_Data.append("Content-Disposition: inline").append("\r\n");
+					// main_Response_Data.append("Content-Disposition: inline").append("\r\n");
 				} else {
 					content_Disposition = "attachment; filename=" + dir_Path + file_Path + ";";
-					main_Response_Data.append("Content-Disposition: ").append(content_Disposition).append("\r\n");
+					// main_Response_Data.append("Content-Disposition:
+					// ").append(content_Disposition).append("\r\n");
 				}
 				if (is_Debug_On) {
 					System.out.println("Content Disposition: " + content_Disposition);
 					System.out.println("Content Type:        " + content_Type);
 
 				}
-				if (!content_Type.equals("text/plain")) {
-					main_Response_Data.append("Cannot read").append(content_Type).append(", type of file!");
+				if (!content_Type.equals("text/plain") && !content_Type.equals("application/json")) {
+					main_Response_Data.append("Cannot read ").append(content_Type).append(", type of file!");
 				} else if (requested_file.exists() && requested_file.isFile()) {
 
 					BufferedReader data_of_file = new BufferedReader(new FileReader(requested_file));
@@ -177,19 +181,19 @@ public class ClientRequestHandler implements Runnable {
 				File requested_file = new File(dir_Path + file_Path.trim());
 				PrintWriter out = null;
 				if (requested_file.exists() && requested_file.isFile()) {
-					if(is_Overwrite) {
+					if (is_Overwrite) {
 						out = new PrintWriter(new FileOutputStream(requested_file, false));
 					} else {
 						out = new PrintWriter(new FileOutputStream(requested_file, true));
-					}					
-					
+					}
+
 				} else {
-					out=new PrintWriter(requested_file);
+					out = new PrintWriter(requested_file);
 				}
 				out.append(data);
 				out.append("\n");
 				out.close();
-			}			
+			}
 		}
 	}
 
@@ -208,25 +212,26 @@ public class ClientRequestHandler implements Runnable {
 	private void makeResponse() throws Exception {
 		if (status_Code == 404) {
 			respond.append("HTTP/1.1 404 NOT FOUND\r\n");
-			main_Response_Data.append("The requested File was not found on the server.\r\n");			
+			main_Response_Data.append("The requested File was not found on the server.\r\n");
 		} else if (status_Code == 403) {
 			respond.append("HTTP/1.1 403 Forbidden\r\n");
 		} else if (status_Code == 400) {
 			respond.append("HTTP/1.1 40 Bad Request\r\n");
-		} else {			
-			respond.append("HTTP/1.1 200 OK\r\n");			
+		} else {
+			respond.append("HTTP/1.1 200 OK\r\n");
 			if (method.equals("post"))
 				main_Response_Data.append("Posted file successfully.");
 		}
-		
+
 		respond.append("Connection: close\r\n");
 		respond.append("Server: httpfs\n");
 		respond.append("Date: ").append(Calendar.getInstance().getTime().toString()).append("\r\n");
 		respond.append("Content-Type: ").append(content_Type).append("\r\n");
 		respond.append("Content-Length: ").append(main_Response_Data.length()).append("\r\n");
+		respond.append("Content-Disposition: ").append(content_Disposition).append("\r\n");
 		respond.append("\r\n");
 		respond.append(main_Response_Data.toString());
-		
+
 	}
 
 }
